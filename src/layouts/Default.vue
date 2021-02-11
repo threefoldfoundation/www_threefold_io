9df0c7ee14613cad4e384f87bdc62024bfb4badc
ehab-hassan <hassane@codescalers.com>
2021-02-11 10:28:12 +0200

    hide nav

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container mx-auto">
-    <NavBar
+    <!-- <NavBar
       :navigation="$static.navigation"
       @setTheme="setTheme"
       :theme="this.theme"
-    />
+    /> -->
     <slot />
     <Footer 
       :record="$static.footer"
