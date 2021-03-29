244d9cfb1946cf02480dc3de40228d30dba2ccf5
ehab-hassan <hassane@codescalers.com>
2021-03-29 11:08:47 +0200

    update brandpanel bg-color

--- src/components/marketing/sections/cta-sections/BrandPanel.vue
+++ src/components/marketing/sections/cta-sections/BrandPanel.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="bg-white">
+  <div class="">
     <div class="max-w-screen-xl mx-auto py-16">
       <div
-        class="brandpanel rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4"
+        class="brandpanel bg-gray-900 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4"
       >
         <div
           class="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-15"
