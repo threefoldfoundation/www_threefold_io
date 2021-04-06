426dee81245319e308624099cf7a116fcd4f0bed
Vilnite <vilnitekristine@gmail.com>
2021-04-06 19:47:29 +0300

    updated buttons in take part section on home

--- src/components/marketing/sections/cta-sections/ShowcaseProductsHome.vue
+++ src/components/marketing/sections/cta-sections/ShowcaseProductsHome.vue
@@ -30,15 +30,15 @@
               v-if="product.url.includes('http')"
               target="_blank"
               :href="product.url"
-              class="bg-gray-800 text-lg text-bold learn-button hover:bg-gray-700 text-gray-100 px-12 py-2 mr-5 shadow rounded-full transition duration-500 ease-in-out"
+              class="bg-blue-900 text-sm learn-button hover:bg-blue-800 text-gray-100 px-12 py-2 mr-5 shadow rounded-full"
               >{{ product.button }}</a
             >
             <g-link
               v-else
               :href="product.url"
-              class="bg-gray-800 text-lg text-bold learn-button hover:bg-gray-700 text-gray-100 px-12 py-2 mr-5 shadow rounded-full transition duration-500 ease-in-out"
+              class="bg-blue-900 text-sm learn-button hover:bg-blue-800 text-gray-100 px-12 py-2 mr-5 shadow rounded-full"
               >{{ product.button }}</g-link
-            >
+            > 
           </div>
         </div>
       </a>
