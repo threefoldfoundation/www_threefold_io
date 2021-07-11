b0d52fcfdcee5df7f50823625d3fd66d59d85df7
kristof de spiegeleer <kristof@incubaid.com>
2021-07-11 12:56:32 +0000

    gitpod

--- gitpod/install.sh
+++ gitpod/install.sh
@@ -23,3 +23,16 @@ npm install --global @gridsome/cli
 cd /workspace
 git clone https://github.com/threefoldfoundation/data_threefold
 
+#go to the website
+cd /workspace/www_threefold_io
+
+rm -f yarn.lock
+rm -rf .cache
+
+#copy from the docker image to speedup
+echo "sync node modules to website"
+rsync -ra --delete $base/node_modules/ node_modules/ 
+echo "done"
+
+#make sure all modules are there
+npm install
--- gitpod/run.sh
+++ gitpod/run.sh
@@ -1,30 +1,15 @@
 set -e
 
-
 export base=/nodejs
-
-
 
 export PATH=$base/bin:$PATH
 
 source $base/nvm.sh
 
-nvm install --lts
 nvm use --lts
 
 #go to the website
-cd ..
-
-rm -f yarn.lock
-rm -rf .cache
-
-#copy from the docker image to speedup
-echo "sync node modules to website"
-rsync -ra --delete $base/node_modules/ node_modules/ 
-echo "done"
-
-#make sure all modules are there
-npm install
+cd /workspace/www_threefold_io
 
 if [ -f vue.config.js ]; then
 npm run-script serve
