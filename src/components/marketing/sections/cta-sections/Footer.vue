44819e15be695714a0feaed7fecedb46133d8ce2
Ehab-hassan <hassane@codescalers.com>
2021-08-09 13:43:23 +0000

    update telegram

--- src/components/marketing/sections/cta-sections/Footer.vue
+++ src/components/marketing/sections/cta-sections/Footer.vue
@@ -99,6 +99,26 @@
                 />
               </svg>
             </a>
+                        <a
+              v-if="record.telegram != '#'"
+              :href="record.telegram"
+              class="text-gray-400 hover:text-gray-500"
+            >
+              <span class="sr-only">telegram</span>
+              <!-- <svg
+                class="h-6 w-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                  clip-rule="evenodd"
+                />
+              </svg> -->
+              <svg data-v-59390861="" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="telegram-plane" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-telegram-plane fa-w-14"><path data-v-59390861="" fill="currentColor" d="M446.7 98.6l-67.6 318.8c-5.1 22.5-18.4 28.1-37.3 17.5l-103-75.9-49.7 47.8c-5.5 5.5-10.1 10.1-20.7 10.1l7.4-104.9 190.9-172.5c8.3-7.4-1.8-11.5-12.9-4.1L117.8 284 16.2 252.2c-22.1-6.9-22.5-22.1 4.6-32.7L418.2 66.4c18.4-6.9 34.5 4.1 28.5 32.2z" class=""></path></svg>
+            </a>
             <a
               v-if="record.dribbble != '#'"
               :href="record.dribbble"
