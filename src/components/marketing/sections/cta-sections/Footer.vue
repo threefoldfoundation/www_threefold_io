29ced8852763e57026af1e933c7ce1021d00069f
samaradel <samar@incubaid.com>
2022-04-27 12:46:02 +0200

    Update footer

--- src/components/marketing/sections/cta-sections/Footer.vue
+++ src/components/marketing/sections/cta-sections/Footer.vue
@@ -255,8 +255,8 @@
         </div>
       </div>
       <div class="mt-12 border-t border-gray-200 pt-8">
-        <p class="text-base leading-6 text-gray-400 xl:text-center">
-          &#xA9; 2021 ThreeFold, All rights reserved.
+        <p class="text-base leading-6 text-gray-500 xl:text-center">
+          ThreeFold S.A. , B265333 &#xA9; 2021 ThreeFold, All rights reserved.
           <a
             class="leading-6 text-gray-500 hover:text-gray-900"
             target="_blank"
