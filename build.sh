d0e823d6996387cade6e163eb9f1a8c06002935c
despiegk <kristof@incubaid.com>
2024-11-23 05:31:33 +0300

    ...

--- build.sh
+++ build.sh
@@ -1,10 +1,46 @@
-#!/bin/bash
-set -ex
+echo "Starting build..."
 
-export BASE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "${BASE}"
+SOURCE=${BASH_SOURCE[0]}
+DIR_OF_THIS_SCRIPT="$( dirname "$SOURCE" )"
+ABS_DIR_OF_SCRIPT="$( realpath $DIR_OF_THIS_SCRIPT )"
+
+# TODO: Check if current version is latest to avoid redundant installation
+if [[ -f "tailwindcss" ]]
+then
+    rm tailwindcss
+fi
+
+# checks os and architecture for correct release
+# https://stackoverflow.com/a/8597411 
+echo "Installing & building tailwind..."
+ASSET="tailwindcss"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    ASSET="$ASSET-linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    ASSET="$ASSET-macos"
+fi
+if [[ "$(uname -m)" == "x86_64"* ]]; then
+    ASSET="$ASSET-x64"
+elif [[ "$(uname -m)" == "arm64"* ]]; then
+    ASSET="$ASSET-arm64"
+fi
+
+curl -sLO "https://github.com/tailwindlabs/tailwindcss/releases/latest/download/${ASSET}"
+chmod +x $ASSET
+mv $ASSET tailwindcss
 
 
-source env.sh
+# initialized and configures tailwind if not configured
+echo "Initializing tailwind..."
+if [[ ! -f "tailwind.config.js" ]]
+then
+    ./tailwindcss init
+    sed -i '' "s|  content: \\[\\],|  content: \\['./templates/**/*.html'\\],|g" tailwind.config.js
+fi
 
-zola serve
+# compiles tailwind css for prod & builds project
+echo "Compiling tailwindcss and building zola project..."
+rm -rf public static/css
+./tailwindcss -i css/index.css -o ./static/css/index.css --minify
+zola --root $ABS_DIR_OF_SCRIPT build
